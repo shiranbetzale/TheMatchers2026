5d8a2f8f8804570a29b2dbddd4128c69dc4eb9ee
Shiran Betzalel <114071838+shiranbetzale@users.noreply.github.com>
2026-07-06 13:06:55 +0300

    Limit lint to JavaScript files

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,3 @@
-const js = require('@eslint/js');
-
 module.exports = [
   {
     ignores: [
@@ -8,36 +6,27 @@ module.exports = [
       'node_modules/**',
       'coverage/**',
       'backend/node_modules/**',
+      '**/*.ts',
+      '**/*.tsx',
       'babel.config.js',
       'metro.config.js',
     ],
   },
-  js.configs.recommended,
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ['**/*.js'],
     languageOptions: {
       ecmaVersion: 'latest',
-      sourceType: 'module',
+      sourceType: 'commonjs',
       globals: {
-        __DEV__: 'readonly',
         console: 'readonly',
-        describe: 'readonly',
-        expect: 'readonly',
-        it: 'readonly',
-        jest: 'readonly',
         module: 'readonly',
         require: 'readonly',
         process: 'readonly',
         setTimeout: 'readonly',
         clearTimeout: 'readonly',
-        FormData: 'readonly',
+        __dirname: 'readonly',
       },
     },
-    rules: {
-      'no-console': 'off',
-      'no-unused-vars': 'off',
-      'no-undef': 'off',
-      'no-empty': 'off',
-    },
+    rules: {},
   },
 ];
